34651edeb4f72711a91a0b395f0fc5fc49027d42
Mert <mertaliyigit06@gmail.com>
2025-01-31 16:03:30 +0300

    in-game hotkey overlay

---
+++ config.lua
@@ -0,0 +1,22 @@
+return {
+    ["flush_keys"] = {
+        key1 = "F",
+        key2 = "Scroll Down",
+    },
+    ["invert_keys"] = {
+        key1 = "S",
+        key2 = "Mouse3",
+    },
+    ["oak_keys"] = {
+        key1 = "D",
+        key2 = "Scroll Up",
+    },
+    ["play_hand_keys"] = {
+        key1 = nil,
+        key2 = nil,
+    },
+    ["discard_hand_keys"] = {
+        key1 = nil,
+        key2 = nil,
+    }
+}
--- init.lua
+++
@@ -1,1 +0,0 @@
-local flushhotkeys = require "flushhotkeys"
